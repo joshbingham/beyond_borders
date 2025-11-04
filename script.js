e8469122248361339cc1851ddbf424a270348871
Josh Bingham <joshuabingham@hotmail.co.uk>
2025-11-04 10:50:03 +0000

    Add JS company object

---
+++ script.js
@@ -0,0 +1,36 @@
+//Create company object
+
+const ourCompany = {
+    name: "Beyond Borders",
+    companyType: "Travel Agency",
+    culture: "Clan",
+    employeeCount: 20,
+    'British Values' : ["Democracy", "The rule of law", "Individual liberty", "Mutual respect", "Tolerance of those with different faiths and beliefs"],
+    foundedYear: 2025,
+    jobRoles: [
+        {
+            title: "Software Engineer",
+            description: "Develop and maintain web applications, connecting back-end to front-end.",
+            skillsRequired: ["JavaScript", "React", "Node.js"],
+        },
+        {
+            title: "Travel Consultant",
+            description: "Assist clients in planning and booking their travel itineraries.",
+            skillsRequired: ["Customer Service", "Attention to Detail", "Knowledge of Travel Destinations"],
+
+        },
+        {
+            title: "Marketing Specialist",
+            description: "Create and implement marketing strategies to promote the company.",
+            skillsRequired: ["SEO", "Content Creation", "Social Media Management"],
+        },
+        {
+            title: "Web Designer",
+            description: "Design user-friendly and visually appealing websites.",
+            skillsRequired: ["HTML", "CSS", "Graphic Design"],
+        }
+    ],
+    headquartersLocation: "Brighton, UK",
+    
+
+}
